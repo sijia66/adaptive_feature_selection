81f7f311ce5d4b16f62ed97c9edfae85349b95a9
sijia66 <sijia66@uw.edu>
2022-01-18 13:37:07 -0800

    new sim files

---
+++ run_feature_distribution.py
@@ -0,0 +1,6 @@
+from simulation_runs import run_iter_feat_addition
+
+data_dump_folder = \
+'/home/sijia-aw/BMi3D_my/operation_funny_chicken/sim_data/theory_dev/'
+
+run_iter_feat_addition(data_dump_folder=data_dump_folder)
