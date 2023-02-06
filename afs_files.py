6b38e5db52dfd28592dbc77bd39a352bda4b56e0
sijia66 <sijia66@uw.edu>
2023-02-06 14:35:34 -0800

    add loading  pickle files

--- afs_files.py
+++ afs_files.py
@@ -2,6 +2,9 @@ import aopy
 import tables
 import os
 
+
+import pickle
+
 def load_feature_selection_files(data_dump_folder, exp_conds):
     """
     load feature selection from data dump folder
@@ -36,3 +39,43 @@ def load_feature_selection_files(data_dump_folder, exp_conds):
 
         
     return (exp_data_all, exp_data_metadata_all)
+
+
+
+def load_clda_pickle_file(data_dump_folder, exp_conds):
+
+    if type(exp_conds) is not list:
+        exp_conds = [exp_conds]
+
+    clda_data_full = []
+
+    for i in exp_conds:
+        pickle_file_name = i + ".p"
+        with open(data_dump_folder + pickle_file_name, 'rb') as f:
+            clda_data =  pickle.load(f)
+        clda_data_full.append(clda_data)
+
+    return clda_data_full
+
+def convert_to_list(clda_data):
+    """
+    convert a list of dictionaries of the keys to a dictionary of lists. 
+    clda_data (list)
+
+    Returns:
+        clda_dict
+    """
+
+    clda_keys = clda_data[0].keys()
+    clda_dict = dict()
+    for k in clda_keys:
+        clda_dict[k] = [c[k] for c in clda_data]
+
+    return clda_dict
+
+
+def load_and_convert_clda_pickle_files(data_dump_folder, exp_conds):
+
+    clda_data_full = load_clda_pickle_file(data_dump_folder, exp_conds)
+    
+    return [convert_to_list(clda_data) for clda_data in clda_data_full]
