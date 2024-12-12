8b8d87d6ef22449c05a44d4f84a6d275c5d3cbb4
sijia66 <sijia66@uw.edu>
2024-12-11 18:18:56 -0800

    changed encoder drawing

--- afs_plotting.py
+++ afs_plotting.py
@@ -188,8 +188,10 @@ def get_cmap(n_lines, color = None):
     return cmap
 
 
-def plot_feature_selection(active_feat_set_list, ax = None, selected_color = 'white', unselected_color = 'black',
-                                                 label_x = True, default_x_label = 'Learner batch',
+def plot_feature_selection(active_feat_set_list, ax = None, 
+                           selected_color = 'white', 
+                           unselected_color = 'black',
+                                                 label_x = True, default_x_label = 'Batch Number',
                                                  label_y = True, default_y_label = 'Feature index'):
     """
     plot the selection strategy map. 
